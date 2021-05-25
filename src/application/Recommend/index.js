a3ce5d90f25cf542a5c54756581407a7152d765a
brightzoe <brightzoe@qq.com>
2021-05-25 11:16:09 +0800

    feat: 🎸 add better-scroll

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Slider from "../../components/slider";
 import RecommendList from "../../components/list";
+import { Content } from "./style";
+import Scroll from "../../components/scroll";
 function Recommend() {
 	//mock 数据
 	const bannerList = [1, 2, 3, 4].map((item) => {
@@ -16,10 +18,14 @@ function Recommend() {
 	});
 
 	return (
-		<div>
-			<Slider bannerList={bannerList}></Slider>
-			<RecommendList recommendList={recommendList}></RecommendList>
-		</div>
+		<Content >
+			<Scroll className="list">
+				<div>
+					<Slider bannerList={bannerList}></Slider>
+					<RecommendList recommendList={recommendList}></RecommendList>
+				</div>
+			</Scroll>
+		 </Content>
 	);
 }
 
--- src/components/scroll/index.js
+++ src/components/scroll/index.js
@@ -1,13 +1,19 @@
-import React, { forwardRef, useState, useRef, useEffect } from "react";
+import React, { forwardRef, useState, useRef, useEffect, useImperativeHandle } from "react";
 import PropTypes from "prop-types";
 import BScroll from "better-scroll";
 import styled from "styled-components";
+const ScrollContainer = styled.div`
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+`;
 const Scroll = forwardRef((props, ref) => {
 	//作为一个通用组件，scroll 组件在业务中会被经常取到原生 DOM 对象，而函数式组件天生不具备被上层组件直接调用 ref 的条件，因此需要用 React 当中一些特殊的方式来处理，即使用 forwardRef 进行包裹。
 	const [bScroll, setBScroll] = useState();
 	const scrollContainerRef = useRef();
-	const { direction, click, refresh, pullUpLoading, pullDownLoading, bounceTop, bounceBottom } = props;
+	const { direction, click, refresh, bounceTop, bounceBottom } = props;
 	const { pullUp, pullDown, onScroll } = props;
+
 	useEffect(() => {
 		//创建better-scroll
 		const scroll = new BScroll(scrollContainerRef.current, {
@@ -19,11 +25,13 @@ const Scroll = forwardRef((props, ref) => {
 				top: bounceTop,
 				bottom: bounceBottom,
 			},
+			// mouseWheel: true,//鼠标滚动，在chrome模拟滚动
 		});
 		setBScroll(scroll);
 		return () => {
 			setBScroll(null);
 		};
+		//eslint-disable-next-line
 	}, []);
 
 	useEffect(() => {
@@ -32,6 +40,7 @@ const Scroll = forwardRef((props, ref) => {
 			bScroll.refresh();
 		}
 	});
+
 	useEffect(() => {
 		//给实例绑定scroll事件
 		if (!bScroll || !onScroll) return;
@@ -42,6 +51,7 @@ const Scroll = forwardRef((props, ref) => {
 			bScroll.off("scroll");
 		};
 	}, [onScroll, bScroll]);
+
 	useEffect(() => {
 		//上拉到底了吗？上拉刷新
 		if (!bScroll || !pullUp) return;
@@ -55,6 +65,7 @@ const Scroll = forwardRef((props, ref) => {
 			bScroll.off("scrollEnd");
 		};
 	}, [pullUp, bScroll]);
+
 	useEffect(() => {
 		//下拉到底了吗？下拉刷新
 		if (!bScroll || !pullDown) return;
@@ -68,7 +79,21 @@ const Scroll = forwardRef((props, ref) => {
 			bScroll.off("touchEnd");
 		};
 	}, [pullDown, bScroll]);
-	return <div></div>;
+
+	useImperativeHandle(ref, () => ({
+		refresh() {
+			if (bScroll) {
+				bScroll.refresh();
+				bScroll.scrollTo(0, 0);
+			}
+		},
+		getBScroll() {
+			if (bScroll) {
+				return bScroll;
+			}
+		},
+	}));
+	return <ScrollContainer ref={scrollContainerRef}> {props.children}</ScrollContainer>;
 });
 
 Scroll.defaultProps = {
