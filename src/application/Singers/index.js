2f745b57a59bc90f3c9c4ab0b69750c7ee7e38a5
brightzoe <brightzoe@qq.com>
2021-07-12 17:53:32 +0800

    feat : 歌手页mock数据和样式

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
 import Horizon from "../../baseUI/horizon-item";
 import { categoryTypes, alphaTypes } from "../../api/config";
-import { NavContainer } from "./style";
+import { NavContainer, ListContainer, List, ListItem } from "./style";
+import Scroll from "../../components/scroll";
+
+const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => {
+	return {
+		picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+		name: "隔壁老樊",
+		accountId: 277313426,
+	};
+});
+
+const renderSingerList = () => {
+	return (
+		<List>
+			{singerList.map((item, index) => {
+				return (
+					<ListItem key={item.accountId + "" + index}>
+						<div className="img_wrapper">
+							<img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music" />
+						</div>
+						<span className="name">{item.name}</span>
+					</ListItem>
+				);
+			})}
+		</List>
+	);
+};
+
 function Singer() {
 	const [category, setCategory] = useState("");
 	const [alpha, setAlpha] = useState("");
@@ -14,10 +41,16 @@ function Singer() {
 	};
 
 	return (
-		<NavContainer>
-			<Horizon list={categoryTypes} title={"分类 (默认热门):"} handleClick={handleUpdateCategory} oldVal={category}></Horizon>
-			<Horizon list={alphaTypes} title={"首字母:"} handleClick={handleUpdateAlpha} oldVal={alpha}></Horizon>
-		</NavContainer>
+		<>
+			<NavContainer>
+				<Horizon list={categoryTypes} title={"分类 (默认热门):"} handleClick={handleUpdateCategory} oldVal={category}></Horizon>
+				<Horizon list={alphaTypes} title={"首字母:"} handleClick={handleUpdateAlpha} oldVal={alpha}></Horizon>
+			</NavContainer>
+			<ListContainer>
+				{" "}
+				<Scroll>{renderSingerList()}</Scroll>
+			</ListContainer>
+		</>
 	);
 }
 export default React.memo(Singer);
