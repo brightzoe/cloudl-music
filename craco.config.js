9522ead7d552f8c3f81b0a3a16bde2846b770c98
brightzoe <brightzoe@qq.com>
2021-07-12 18:33:08 +0800

    添加craco

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const pathResolve = (pathUrl) => path.join(__dirname, pathUrl);
+
+module.exports = {
+	webpack: {
+		alias: {
+			"@": pathResolve("src"),
+		},
+	},
+	babel: {
+		plugins: [
+			"babel-plugin-styled-components",
+			{
+				ssr: false,
+			},
+			{
+				displayName: false,
+			},
+		],
+	},
+};
