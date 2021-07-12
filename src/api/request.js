f9b3bde15b1a7d1271a909f814ad91f65c1a9a7b
brightzoe <brightzoe@qq.com>
2021-07-12 17:57:44 +0800

    refeator: api添加

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,21 @@
 import { axiosInstance } from "./config";
+
+//推荐页banner
 export const getBannerRequest = () => {
 	return axiosInstance.get("/banner");
 };
+
+//推荐页歌曲数据
 export const getRecommendListRequest = () => {
 	return axiosInstance.get("./personalized");
+};
+
+//歌手页hot歌手数据
+export const getHotSingerListRequest = (count) => {
+	return axiosInstance.get(`/top/artists?offset=${count}`);
+};
+
+//歌手页数据
+export const getSingerListRequest = (category, alpha, count) => {
+	return axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
 };
